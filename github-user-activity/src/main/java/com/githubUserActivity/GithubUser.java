8456c5e3e9cfaf797c96991ec724eb02af530941
GraMmuLa <poliferik@gmail.com>
2025-05-04 15:56:07 +0200

    Initialized Project with Maven

---
+++ github-user-activity/src/main/java/com/githubUserActivity/GithubUser.java
@@ -0,0 +1,12 @@
+package main.java.com.githubUserActivity;
+
+import java.util.List;
+
+public class GithubUser {
+    private long id;
+    private String username;
+
+    private List<String> activity;
+
+
+}
---
+++ github-user-activity/src/main/java/com/githubUserActivity/Main.java
@@ -0,0 +1,10 @@
+package main.java.com.githubUserActivity;
+
+/**
+ * Hello world!
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
